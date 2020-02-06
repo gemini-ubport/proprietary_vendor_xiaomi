a6ff6cc35e9dbf8e6b9eaf8965e6acb8b16bc08d
NeKit <nekit1000@gmail.com>
2020-02-07 00:40:42 +0100

    msm8996-common: Do not pull Java dependencies into build
    Change-Id: If778dc1c21bd5498c74bbc409c51192ac07dfd92
    Signed-off-by: iKeramat <ikeramat80@gmail.com>

--- msm8996-common/msm8996-common-vendor.mk
+++ msm8996-common/msm8996-common-vendor.mk
@@ -634,16 +634,16 @@ PRODUCT_PACKAGES += \
     libantradio \
     libsdm-disp-vndapis \
     libthermalclient \
-    QtiTelephonyService \
-    uceShimService \
-    CNEService \
-    SmartcardService \
-    WfdService \
-    dpmserviceapp \
-    ims \
-    qcrilmsgtunnel \
-    PowerOffAlarm \
-    TimeService \
-    WfdCommon \
     ReferenceFeature \
     chargeonlymode
+#    uceShimService \
+#    QtiTelephonyService \
+#    CNEService \
+#    SmartcardService \
+#    WfdService \
+#    dpmserviceapp \
+#    ims \
+#    qcrilmsgtunnel \
+#    PowerOffAlarm \
+#    TimeService \
+#    WfdCommon \
